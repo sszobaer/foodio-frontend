b1a41a894e79677594a38bc03585a350c774dcbf
SSZASeyam <118447914+sszobaer@users.noreply.github.com>
2026-03-17 01:01:18 +0600

    My Order page Authentication bug fixed

--- src/app/(public)/my-orders/page.tsx
+++ src/app/(public)/my-orders/page.tsx
@@ -1,7 +1,17 @@
 import { getMyOrders } from "@/services/public/get-my-orders.service";
 import MyOrdersSection from "@/components/features/public/orders/MyOrdersSection";
 
-export default async function MyOrdersPage() {
-    const orders = await getMyOrders();
+
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+export default async function OrdersPage() {
+  const cookieStore = await cookies();   // <-- FIX
+  const token = cookieStore.get("access_token")?.value;
+
+  if (!token) {
+    redirect("/sign-in");
+  }
+const orders = await getMyOrders();
     return <MyOrdersSection orders={orders} />;
 }
